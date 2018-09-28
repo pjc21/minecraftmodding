cf4e06bf9ec09eed5895bc1728b04b04100c4ec6
pjc21 <craik.paul@gmail.com>
2018-09-29 00:39:06 +1000

    Add files via upload

--- java/pjc21/mod/objects/blocks/slots/SlotSinteringFurnaceInputs.java
+++ java/pjc21/mod/objects/blocks/slots/SlotSinteringFurnaceInputs.java
@@ -17,45 +17,7 @@ public class SlotSinteringFurnaceInputs extends SlotItemHandler
 	@Override
     public boolean isItemValid(@Nonnull ItemStack stack)
     {
-		
 		return SinteringFurnaceRecipes.getInstance().isValidRecipeItem(stack);
-        /*if (stack.isEmpty())
-        {
-        	return false;
-        }
-
-        IItemHandler handler = this.getItemHandler();
-        
-        if(handler.getStackInSlot(0).isEmpty() && handler.getStackInSlot(1).isEmpty())
-        {
-        	if(SinteringFurnaceRecipes.getInstance().isValidRecipeItem(stack))
-            {
-            	return true;
-            }
-        }
-        else if(handler.getStackInSlot(0).isEmpty() || handler.getStackInSlot(1).isEmpty())
-        {
-    		if(SinteringFurnaceRecipes.getInstance().isValidRecipeItem(stack))
-			{
-				if(handler.getStackInSlot(0).isEmpty())
-	        	{
-					ItemStack slot1 = handler.getStackInSlot(1);
-					if(!SinteringFurnaceRecipes.getInstance().getSinteringResult(stack, slot1).isEmpty())
-					{
-						return true;
-					}
-	        	}
-				else if(handler.getStackInSlot(1).isEmpty())
-				{
-					ItemStack slot0 = handler.getStackInSlot(0);
-					if(!SinteringFurnaceRecipes.getInstance().getSinteringResult(stack, slot0).isEmpty())
-					{
-						return true;
-					}
-				}
-			}
-        }
-        return false;*/
     }
 	
 	@Override
